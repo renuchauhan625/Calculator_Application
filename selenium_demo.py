b292722d4d8e6735a2b9d0196899d4eade82a9c3
unknown <renuchauhan068#gmail.com>
2020-11-03 15:26:08 +0530

    Added Application in Calculator

---
+++ selenium_demo.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+driver=webdriver.Chrome(executable_path="C:\\Users\Renu\\Downloads\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("http://127.0.0.1:8000/calci")
+driver.maximize_window()
+driver.find_element_by_id("n1").send_keys("1")
+driver.find_element_by_id("n2").send_keys("2")
+select=Select(driver.find_element_by_id("op"))
+time.sleep(1)
+select.select_by_value('add')
+time.sleep(2)
+driver.find_element_by_id("b1").click()
+time.sleep(5)
+driver.quit()
+
