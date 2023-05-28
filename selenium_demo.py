ff3ab1e9a9c494e83d51b61fa38f47cb93a2f1c7
Renu Chauhan <61846916+renuchauhan625@users.noreply.github.com>
2023-05-28 23:28:00 +0530

    Update selenium_demo.py

--- selenium_demo.py
+++ selenium_demo.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import Select
 driver=webdriver.Chrome(executable_path="C:\\Users\Renu\\Downloads\\chromedriver_win32\\chromedriver.exe")
-
 driver.get("http://127.0.0.1:8000/calci")
 driver.maximize_window()
 driver.find_element_by_id("n1").send_keys("1")
